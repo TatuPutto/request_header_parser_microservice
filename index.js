cb24b360e3125b7b4af6afa3e266917587857960
TatuPutto <tatuputto@gmail.com>
2017-03-19 19:23:08 +0200

    Show ip address in IPv4 format

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const ipaddr = require('ipaddr.js');
 const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
 
 app.get('/', (req, res) => {
-    const ipaddress = req.connection.remoteAddress;
+    const ipaddress = req.ip;
     const language = req.headers['accept-language'].split(',')[0];
     const software = req.headers['user-agent'].split('(')[1].split(')')[0];
     const output = {ipaddress, language, software};
-
+    
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(output));
 });
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), '127.0.0.1');
